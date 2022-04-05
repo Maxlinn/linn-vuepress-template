5fffad6a6cebdb74f20d55eaf1f0b1f692a95615
Maxlinn <maxlinn@foxmail.com>
2022-04-05 20:18:04 +0800

    version 1.0

---
+++ .vuepress/config.js
@@ -0,0 +1,27 @@
+// https://vuepress.vuejs.org/zh/config/
+// https://www.vuepress.cn/zh/theme/default-theme-config.html
+
+module.exports = {
+    // if you want to deploy to 'http://xxx/ instead of 'http://xxx/yy', remove next line
+    base: "/linn-vuepress-template/",
+    title: "This is site title",
+    description: "This is invisible HTML description",
+
+    themeConfig: {
+        nav: [
+            {
+                text: 'CC-BY-SA 4.0',
+                link: 'https://creativecommons.org/licenses/by-sa/4.0'
+            }
+        ],
+        sidebar: 'auto',
+
+        // if you are using github... 
+        // Last updated prefix in the bottom of page
+        lastUpdated: 'Last Updated',
+        repo: 'Maxlinn/linn-vuepress-template',
+        repoLabel: 'Github',
+        editLinks: true,
+        editLinkText: 'Suggest This Page'
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# linn-vuepress-template
+# This is README.md
+
 Quickly setup a simple site to store markdown notes, and share them via static website.
+
+## Usage
+
+- Uses this template to create a repo.
+- Modify `.vuepress/config.js`, guided by comments inside.
+- (Optional) Modify `.github/workflows/vuepress-deploy.yml` to suit your deployment.
+- Turn on `github pages` inside settings.
+- Write your markdown files. Upon your push, the static site will be auto generated(may take time to effect due to github).
+  - Remember, this `README.md` will appear as `index.html` afterwards, so don't hesitate to replace it with your own!
+
+
+
+## Todo
+
+- [ ] Auto generate sidebar by directory.
+- [ ] Add dark mode.
+- [ ] Add back to top.
+- [ ] Add `.gitignore`
+- [ ] Add instructions to install vuepress.
+- [ ] Add full-text searching.
