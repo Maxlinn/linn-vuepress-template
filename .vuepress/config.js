0746c1d78d1ed4e65a94f93ff3517673d888ddbf
Maxlinn <maxlinn@foxmail.com>
2022-04-06 16:26:18 +0800

    diabled vuepress-bar for now

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,8 +1,8 @@
 // https://vuepress.vuejs.org/zh/config/
 // https://www.vuepress.cn/zh/theme/default-theme-config.html
 
-const getConfig = require("vuepress-bar")
-const { _, sidebar } = getConfig(".vuepress/..")
+// const getConfig = require("vuepress-bar")
+// const { _, sidebar } = getConfig(".vuepress/..")
 
 module.exports = {
     // if you want to deploy to 'http://xxx/ instead of 'http://xxx/yy', remove next line
@@ -23,15 +23,11 @@ module.exports = {
                 link: 'https://creativecommons.org/licenses/by-sa/4.0'
             }
         ],
-        // sidebar: 'auto', // 
+        sidebar: 'auto', // 
         // root level documents must be added manually
-        sidebar: [
-            {
-                title: "Home",
-                children: ["README.md"]
-            },
-            ...sidebar.filter(i => i.title !== "Node Modules")
-        ],
+        // sidebar: [
+        //     ...sidebar.filter(i => i.title !== "Node Modules")
+        // ],
         smoothScroll: true,
 
         /// if you are using github... 
